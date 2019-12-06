d702f0bee40461e663a5c714fe0036e93e9393ef
anavision <89278489696@mail.ru>
2019-12-06 20:30:52 +0300

    Домашняя работа. Нарисовал...

---
+++ lab3/primer1.py
@@ -0,0 +1,14 @@
+from graph import *
+
+penColor(249,132,229)
+penSize(5)
+brushColor("blue")
+rectangle(100, 100, 300, 200)
+brushColor("yellow")
+polygon([(100,100), (200,50),
+         (300,100), (100,100)])
+penColor("white")
+brushColor("green")
+circle(200, 150, 50)
+
+run()
---
+++ lab3/primer3.py
@@ -0,0 +1,28 @@
+from graph import *
+
+
+# line(0, 0, 20, 600)
+
+def krug(a, b, c, d, e, f, g, o, p):
+    """
+    risuem krug
+    a,b,c -rgb цвет линии
+    d,e,f - цвет заливки
+    g,o, - координаты круга, p - радиус
+
+    """
+    penColor(a, b, c)
+    brushColor(d, e, f)
+    circle(g, o, p)
+    return ()
+
+
+krug(160, 60, 60, 56, 102, 70, 250, 200, 150) # bashka
+
+krug(250, 21, 193, 255, 120, 40, 200, 130, 25)  #glaza
+krug(250, 21, 193, 255, 120, 40, 300, 130, 25)  #glaza
+krug(250, 21, 193, 0, 0, 0, 200, 130, 10)  #glaza
+krug(250, 21, 193, 0, 0, 0, 300, 130, 10)  #glaza
+
+
+run()
