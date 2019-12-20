bbdf73ea9a1911baa9d12d9ad2e3945f10f4f7bf
anavision <89278489696@mail.ru>
2019-12-20 21:25:19 +0300

    dorabotka

--- lab3/primer3.py
+++ lab3/primer3.py
@@ -1,15 +1,12 @@
 from graph import *
 
 
-# line(0, 0, 20, 600)
-
 def krug(a, b, c, d, e, f, g, o, p):
     """
     risuem krug
     a,b,c -rgb цвет линии
     d,e,f - цвет заливки
     g,o, - координаты круга, p - радиус
-
     """
     penColor(a, b, c)
     brushColor(d, e, f)
@@ -17,15 +14,17 @@ def krug(a, b, c, d, e, f, g, o, p):
     return ()
 
 
-krug(160, 60, 60, 56, 102, 70, 250, 200, 150) # bashka
-
+krug(160, 60, 60, 56, 102, 70, 250, 200, 150)   # bashka
 krug(250, 21, 193, 255, 120, 40, 200, 130, 25)  #glaza
-krug(250, 21, 193, 255, 120, 40, 300, 130, 25)  #glaza
-krug(250, 21, 193, 0, 0, 0, 200, 130, 10)  #glaza
-krug(250, 21, 193, 0, 0, 0, 300, 130, 10)  #glaza
+krug(250, 201, 193, 255, 120, 40, 300, 130, 25)  #praviyglaz
+krug(250, 21, 193, 0, 0, 0, 200, 130, 10)  #zrachki
+krug(250, 21, 193, 0, 0, 0, 300, 130, 10)  #zrachki
 
 
-rectangle(100, 150, 120, 100)   # hren udalit
+rectangle(100, 10, 150, 60)
 
-rectangle(300, 100, 120, 100)
+#hren rervdfvfddfvdfvdfffffffffffffff
+
+#rectangle(300, 100, 120, 100)
+
 run()
